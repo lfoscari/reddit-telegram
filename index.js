436684f7a53fae1c15087063bb69401da4507f11
Paolo Foscari <paolo.foscari@axenso.com>
2018-08-15 22:46:50 +0200

    Js fix

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const Telegraf = require('telegraf');
-const r = require("nraw");
+"use strict";
 
-const bot = new Telegraf('681501793:AAE5KzNwHPHqiCG0WKOG3wDEg-_6mDqPaBg', {
+var subreddit = "wtfstockphotos";
+var postEveryHours = 24; // one post per day
+
+var Telegraf = require('telegraf');
+var r = require("nraw");
+
+var bot = new Telegraf('681501793:AAE5KzNwHPHqiCG0WKOG3wDEg-_6mDqPaBg', {
   channelMode: true
 });
 
-// bot.start((message) => {
-//   console.log('started:', message.from.id)
-//   return message.reply('Hello my friend, contact me by send /contact, or write anything');
-// })
-
 var Reddit = new r("Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0");
 
-bot.command('start', message => {
+bot.command('start', function (message) {
   setInterval(function () {
-    Reddit.subreddit("wtfstockphotos").random().exec(function(res){
-      var data = res[0].data.children[0].data
+    Reddit.subreddit(subreddit).random().exec(function (res) {
+      var data = res[0].data.children[0].data;
       console.log(data.title + "\n" + data.author + "\n" + data.url + "\n");
       message.replyWithPhoto(data.url);
-    })
-  }, 1000 * 60 * 60); // hour
+    });
+  }, 1000 * 60 * 60 * postEveryHours); // 24 hour
 });
 
 bot.startPolling();
